1a792e6400c2f6647e3ae0f37842dedcc8663497
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2021-09-02 09:56:24 +0700

    Add project files.

---
+++ Business/EFManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFPractice.Business {
+  class EFManager {
+    internal EFManager() { }
+
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace EFPractice {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
