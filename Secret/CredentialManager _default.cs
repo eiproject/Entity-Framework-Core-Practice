8fda87f5bbb7607fc3b27c623d6b35bf8f4d9c47
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2021-09-02 09:58:35 +0700

    hide secret things

---
+++ Secret/CredentialManager _default.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataManager.Business {
+  public class CredentialManager_Default {
+    internal string DBUsername = "";
+    internal string DBPassword = "";
+
+    internal string DBTunnelHost = "";
+    internal int DBTunnelPort = 0;
+    internal string DBTunnelUser = "";
+    internal string DBTunnelPassword = "";
+    
+    internal string DBLocalServer = "";
+    internal int DBLocalPort = 0;
+    internal string DBName = "";
+  }
+}
